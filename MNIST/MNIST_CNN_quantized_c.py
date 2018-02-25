d1708524e9ee1e025430a231ec2be9e2958592e0
Sean <seanfanning00@gmail.com>
2018-02-25 14:30:04 +0000

    Adding noise to quantized convolutional layers

--- MNIST/MNIST_CNN_quantized_c.py
+++ MNIST/MNIST_CNN_quantized_c.py
@@ -239,6 +239,7 @@ def train():
         #variable_summaries(quantized_biases)
         quantized_biases = fake_quantize_tensor(biases, bits_b, -max_b, max_b, name="quantized_biases")
         # quantized_biases = biases
+
       with tf.name_scope('quantized_Wx_plus_b'):
         preactivate_q = tf.matmul(input_tensor, quantized_weights) + quantized_biases + noise
         #quantized_preactivate = tf.fake_quant_with_min_max_args(preactivate_q, -quantization_range/2, quantization_range/2, quantization_bits, narrow_range=False, name='quantized_weights')
@@ -336,9 +337,55 @@ def train():
 
       return quantized_out_layer
 
+  # TODO: Create a Conv layer with Quantized variables, adding random noise
+  def conv_layer_quantized_add_noise(input_data, num_input_channels, num_filters, filter_shape, pool_shape, bits_w, max_w, bits_b, max_b, bits_a, max_a, layer_name):
+    with tf.name_scope(layer_name):
+      # setup the filter input shape for tf.nn.conv_2d
+      conv_filt_shape = [filter_shape[0], filter_shape[1], num_input_channels, num_filters]
+
+      # initialise weights and bias for the filter
+      with tf.name_scope('weights'):
+        weights = weight_variable(conv_filt_shape)
+        variable_summaries(weights)
+      with tf.name_scope('biases'):
+        # biases = bias_variable([num_filters])
+        biases = weight_variable([num_filters])
+        variable_summaries(biases)
+      with tf.name_scope('additive_noise'): # Add some noise to move some nodes into activation if they are close
+        noise = tf.random_normal([output_dim], mean=0.0, stddev=noise_stddev)
+        variable_summaries(noise)
+
+      # Quantization
+      with tf.name_scope('quantized_weights'):
+        quantized_weights = fake_quantize_tensor(weights, bits_w, -max_w, max_w, name="quantized_weights")
+      with tf.name_scope('quantized_biases'):
+        quantized_biases = fake_quantize_tensor(biases, bits_b, -max_b, max_b, name="quantized_biases")
+
+      with tf.name_scope('out_layer'):
+        # setup the convolutional layer operation
+        out_layer = tf.nn.conv2d(input_data, quantized_weights, [1, 1, 1, 1], padding='SAME')
+        out_layer = out_layer + quantized_biases + noise
+        quantized_out_layer = fake_quantize_tensor(out_layer, bits_a, -max_a, max_a, name="quantized_out_layer")
+        # apply a ReLU non-linear activation
+        quantized_out_layer = tf.nn.relu(quantized_out_layer)
+
+      # now perform max pooling
+      # ksize is the argument which defines the size of the max pooling window (i.e. the area over which the maximum is
+      # calculated).  It must be 4D to toe of frogmatch the convolution - in this case, for each image we want to use a 2 x 2 area
+      # applied to each channel
+      ksize = [1, pool_shape[0], pool_shape[1], 1]
+      # strides defines how the max pooling area moves through the image - a stride of 2 in the x direction will lead to
+      # max pooling areas starting at x=0, x=2, x=4 etc. through your image.  If the stride is 1, we will get max pooling
+      # overlapping previous max pooling areas (and no reduction in the number of parameters).  In this case, we want
+      # to do strides of 2 in the x and y directions.
+      strides = [1, 2, 2, 1]
+      quantized_out_layer = tf.nn.max_pool(quantized_out_layer, ksize=ksize, strides=strides, padding='SAME')
+
+      return quantized_out_layer
+
   # with tf.name_scope('ConvolutionLayers'):
-  layer1 = conv_layer_quantized(image_shaped_input, 1, 32, [5, 5], [2, 2], conv1_w_bits, conv1_w_max, conv1_b_bits, conv1_b_max, conv1_a_bits, conv1_a_max, layer_name='conv1')
-  layer2 = conv_layer_quantized(layer1, 32, 64, [5, 5], [2, 2], conv2_w_bits, conv2_w_max, conv2_b_bits, conv2_b_max, conv2_a_bits, conv2_a_max, layer_name='conv2')
+  layer1 = conv_layer_quantized_add_noise(image_shaped_input, 1, 32, [5, 5], [2, 2], conv1_w_bits, conv1_w_max, conv1_b_bits, conv1_b_max, conv1_a_bits, conv1_a_max, layer_name='conv1')
+  layer2 = conv_layer_quantized_add_noise(layer1, 32, 64, [5, 5], [2, 2], conv2_w_bits, conv2_w_max, conv2_b_bits, conv2_b_max, conv2_a_bits, conv2_a_max, layer_name='conv2')
 
   with tf.name_scope('flatten'):
     x_flattened = tf.reshape(layer2, [-1, 7*7*64])
