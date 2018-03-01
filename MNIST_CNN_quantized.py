7c81a98657cb907241062e8627bf45c1a8042d02
Sean <seanfanning00@gmail.com>
2018-03-01 19:59:08 +0000

    Tuning min and max quantization bounds

--- MNIST_CNN_quantized.py
+++ MNIST_CNN_quantized.py
@@ -27,62 +27,62 @@ noise_enabled_fc = True
 noise_enabled_conv = True
 
 # Conv 1
-conv1_w_bits = 2
+conv1_w_bits = 4
 conv1_w_min = -0.3
 conv1_w_max = 0.3
-conv1_b_bits = 2
+conv1_b_bits = 4
 conv1_b_min = -0.3
 conv1_b_max = 0.3
 conv1_a_bits = 4
-conv1_a_min = -8
-conv1_a_max = 8
+conv1_a_min = -1
+conv1_a_max = 1
 
 # Conv 2
-conv2_w_bits = 2
+conv2_w_bits = 4
 conv2_w_min = -0.3
 conv2_w_max = 0.3
-conv2_b_bits = 2
+conv2_b_bits = 4
 conv2_b_min = -0.3
 conv2_b_max = 0.3
 conv2_a_bits = 4
-conv2_a_min = -8
-conv2_a_max = 8
+conv2_a_min = -1
+conv2_a_max = 1
 
 # Fully Connected 1
-fc1_depth = 1000
-fc1_w_bits = 2
+fc1_depth = 250
+fc1_w_bits = 4
 fc1_w_min = -0.3
 fc1_w_max = 0.3
-fc1_b_bits = 2
+fc1_b_bits = 4
 fc1_b_min = -0.3
 fc1_b_max = 0.3
 fc1_a_bits = 4
-fc1_a_min = -8
-fc1_a_max = 8
+fc1_a_min = -2
+fc1_a_max = 2
 
 # Fully Connected 2 (OUTPUT)
 fc2_depth = 10
-fc2_w_bits = 2
+fc2_w_bits = 4
 fc2_w_min = -0.3
 fc2_w_max = 0.3
-fc2_b_bits = 2
+fc2_b_bits = 4
 fc2_b_min = -0.3
 fc2_b_max = 0.3
 fc2_a_bits = 4
-fc2_a_min = -8
-fc2_a_max = 8
+fc2_a_min = -2
+fc2_a_max = 2
 
 # Fully Connected 3 (MIDDLE)
-fc3_depth = 500
-fc3_w_bits = 2
+fc3_depth = 250
+fc3_w_bits = 4
 fc3_w_min = -0.3
 fc3_w_max = 0.3
-fc3_b_bits = 2
+fc3_b_bits = 4
 fc3_b_min = -0.3
 fc3_b_max = 0.3
 fc3_a_bits = 4
-fc3_a_min = -8
-fc3_a_max = 8
+fc3_a_min = -2
+fc3_a_max = 2
 
 
 def train():
@@ -158,14 +158,14 @@ def train():
   elif(num_layers == 1):
     keep_prob = tf.placeholder(tf.float32)  # Need to create placeholders
     keep_prob2 = tf.placeholder(tf.float32) # Even though they wont be used
-    y = create_fc_layer(x_flattened, 7*7*64, 10, fc2_w_bits, fc2_w_max, fc2_b_bits, fc2_b_max, fc2_a_bits, fc2_a_max, noise_stddev, layer_name='fully_connected', act=tf.identity)
+    y = create_fc_layer(x_flattened, 7*7*64, fc3_depth, fc2_w_bits, fc2_w_max, fc2_b_bits, fc2_b_max, fc2_a_bits, fc2_a_max, noise_stddev, layer_name='fully_connected', act=tf.identity)
 
   else: # Otherwise create 2 layers
     # hidden1 = fc_layer_quantized_add_noise(x_flattened, 7*7*64, fc1_depth, fc1_w_bits, fc1_w_max, fc1_b_bits, fc1_b_max, fc1_a_bits, fc1_a_max, noise_stddev, 'fully_connected1')
     # hidden1 = fc_layer_quantized(x_flattened, 7 * 7 * 64, fc1_depth, fc1_w_bits, fc1_w_max, fc1_b_bits, fc1_b_max, fc1_a_bits, fc1_a_max, 'fully_connected1')
     # hidden1 = fc_layer(x_flattened, 7 * 7 * 64, fc1_depth, 'fully_connected1')
 
-    hidden1 = create_fc_layer(x_flattened, 7 * 7 * 64, fc1_depth, fc1_w_bits, fc1_w_max, fc1_b_bits, fc1_b_max,
+    hidden1 = create_fc_layer(x_flattened, 7*7*64, fc1_depth, fc1_w_bits, fc1_w_max, fc1_b_bits, fc1_b_max,
                               fc1_a_bits, fc1_a_max, noise_stddev, 'fully_connected1')
 
     with tf.name_scope('dropout'):
@@ -259,11 +259,11 @@ def train():
         print('Accuracy at step %s: %s' % (i, acc))
 
 
-  print("Training Completed\t", datetime.datetime.now().strftime("%H:%M:%S"))
+  print("Training Completed: ", datetime.datetime.now().strftime("%H:%M:%S"))
   summary, acc = sess.run([merged, accuracy], feed_dict=feed_dict(False))
   test_writer.add_summary(summary, FLAGS.max_steps + 1)
   test_writer.close()
-  print('Accuracy at step ', FLAGS.max_steps+1, ': %s' % (acc))
+  print('Accuracy at step ', FLAGS.max_steps, ': %s' % (acc))
 
 
   # # Add ops to save and restore all the variables.
@@ -297,7 +297,7 @@ if __name__ == '__main__':
   parser.add_argument('--fake_data', nargs='?', const=True, type=bool,
                       default=False,
                       help='If true, uses fake data for unit testing.')
-  parser.add_argument('--max_steps', type=int, default=15000,
+  parser.add_argument('--max_steps', type=int, default=10000,
                       help='Number of steps to run trainer.')
   parser.add_argument('--learning_rate', type=float, default=0.001,
                       help='Initial learning rate')
